b1aaf28ef79c484f0b2adca7d2fab923a8a8df98
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-05-30 01:18:54 +0700

    Create bottom sheet search city

---
+++ app/src/main/java/com/project/skypass/presentation/home/search/SearchFragment.kt
@@ -0,0 +1,22 @@
+package com.project.skypass.presentation.home.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.project.skypass.databinding.FragmentSearchBinding
+
+class SearchFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
