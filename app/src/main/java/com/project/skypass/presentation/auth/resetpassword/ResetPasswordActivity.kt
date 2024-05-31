9bcea329569b4b6579df30c1fabc3c4fc6f4fefc
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-05-31 18:15:30 +0700

    Layout home done all!

---
+++ app/src/main/java/com/project/skypass/presentation/auth/resetpassword/ResetPasswordActivity.kt
@@ -0,0 +1,17 @@
+package com.project.skypass.presentation.auth.resetpassword
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project.skypass.databinding.ActivityResetPasswordBinding
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    private val binding : ActivityResetPasswordBinding by lazy {
+        ActivityResetPasswordBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
