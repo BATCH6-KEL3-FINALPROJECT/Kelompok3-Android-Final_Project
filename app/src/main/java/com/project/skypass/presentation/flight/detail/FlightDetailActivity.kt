83e1c65eb0fc93cf3ac286dd6a4be8cc454198a4
GigihDC <112244673+GigihDC@users.noreply.github.com>
2024-05-31 07:30:06 +0700

    Creating Layout Flight Data

---
+++ app/src/main/java/com/project/skypass/presentation/flight/detail/FlightDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.project.skypass.presentation.flight.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.project.skypass.R
+
+class FlightDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_flight_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
