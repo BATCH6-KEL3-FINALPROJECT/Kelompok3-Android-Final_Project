c78df074242bf8dc8832eb2ade7326530d69ac59
GigihDC <112244673+GigihDC@users.noreply.github.com>
2024-06-11 20:23:28 +0700

    Update filter on flight data

--- app/src/main/java/com/project/skypass/presentation/flight/filter/FilterFragment.kt
+++ app/src/main/java/com/project/skypass/presentation/flight/filter/FilterFragment.kt
@@ -1,23 +1,31 @@
 package com.project.skypass.presentation.flight.filter
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.project.skypass.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.project.skypass.databinding.FragmentFilterBinding
 
-class FilterFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class FilterFragment : BottomSheetDialogFragment() {
+    private lateinit var  binding: FragmentFilterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter, container, false)
+    ): View {
+        binding = FragmentFilterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+//        binding.ivClose.setOnClickListener {
+//            dismiss()
+//        }
     }
 }
