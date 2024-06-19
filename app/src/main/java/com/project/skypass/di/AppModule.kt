bba388b30a6bba6099c8f1e03c048592cdac6e67
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-06-19 15:14:43 +0700

    Add selector in calendar

--- app/src/main/java/com/project/skypass/di/AppModule.kt
+++ app/src/main/java/com/project/skypass/di/AppModule.kt
@@ -84,6 +84,7 @@ import com.project.skypass.presentation.profile.settingaccount.SettingsAccountVi
 import com.project.skypass.presentation.flight.detail.FlightDetailViewModel
 import com.project.skypass.presentation.flight.result.FlightResultViewModel
 import com.project.skypass.presentation.history.HistoryViewModel
+import com.project.skypass.presentation.home.calendar.CalendarHomeViewModel
 import com.project.skypass.presentation.profile.ProfileViewModelExample
 import com.project.skypass.utils.SharedPreferenceUtils
 import org.koin.android.ext.koin.androidContext
@@ -236,6 +237,7 @@ object AppModule {
         viewModelOf(::ChangeProfileViewModelExample)
         viewModelOf(::ProfileViewModelExample)
         viewModelOf(::HistoryViewModel)
+        viewModelOf(::CalendarHomeViewModel)
         viewModelOf(::CheckoutSeatViewModel)
     }
 
