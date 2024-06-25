299d07de1f5b57cc6ce1f7c77ac3963bc9189e9c
rian syah <rsyah1641@gmail.com>
2024-06-26 01:44:34 +0700

    add feature destination to flight ticket

--- app/src/main/java/com/project/skypass/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/project/skypass/presentation/home/HomeViewModel.kt
@@ -1,18 +1,34 @@
 package com.project.skypass.presentation.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.project.skypass.data.model.OrderUser
+import com.project.skypass.data.model.User
 import com.project.skypass.data.repository.OrderHistory.OrderHistoryRepository
 import com.project.skypass.data.repository.favoritedestination.FavoriteDestinationRepository
+import com.project.skypass.data.repository.pref.PrefRepository
+import com.project.skypass.data.repository.user.UserRepository
+import com.project.skypass.utils.ResultWrapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-class HomeViewModel(private val favoriteRepository: FavoriteDestinationRepository, private val orderHistoryRepository: OrderHistoryRepository): ViewModel() {
+class HomeViewModel(
+    private val favoriteRepository: FavoriteDestinationRepository,
+    private val orderHistoryRepository: OrderHistoryRepository,
+    private val prefRepository: PrefRepository,
+    private val userRepository: UserRepository,): ViewModel() {
     fun getFavoriteDestination() = favoriteRepository.getFavoriteDestination()
     fun getAllOrderHistory() = orderHistoryRepository.getUserOrderHistoryData().asLiveData(Dispatchers.IO)
-    fun removeCart(item: OrderUser) = orderHistoryRepository.deleteOrderHistory(item).asLiveData(Dispatchers.IO)
     fun deleteAllOrderHistory() = orderHistoryRepository.deleteAllOrderHistory().asLiveData(Dispatchers.IO)
+
+    fun getUserId(): String {
+        return prefRepository.getUserID()
+    }
+
+    fun showDataUser(id: String): LiveData<ResultWrapper<User>> {
+        return userRepository.getUser(id).asLiveData(Dispatchers.IO)
+    }
 }
